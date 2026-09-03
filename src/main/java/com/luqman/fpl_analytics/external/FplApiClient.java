d03a7922ba92448b1ec58c12317843a393de9ff1
LuqmanMC305 <luqmanulhakim0003@gmail.com>
2026-09-03 21:24:01 +0800

    Added external api

--- src/main/java/com/luqman/fpl_analytics/external/FplApiClient.java
+++ src/main/java/com/luqman/fpl_analytics/external/FplApiClient.java
@@ -1,11 +1,17 @@
 package com.luqman.fpl_analytics.external;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
 
 @Component 
 public class FplApiClient {
+
+    private final RestClient restClient = RestClient.create();
     
-    public String getPlayerByName(String name){
-        return "Mock Player" + name;
+    public String getRawData(){
+        return restClient.get()
+        .uri("https://fantasy.premierleague.com/api/bootstrap-static/")
+        .retrieve()
+        .body(String.class);
     }
 }
