3935549126790f5da1dea83678fc1be1d97dee08
LuqmanMC305 <luqmanulhakim0003@gmail.com>
2026-09-04 20:18:02 +0800

    Added jackson to seamlessly convert json to java objects

--- src/main/java/com/luqman/fpl_analytics/external/fpl/dto/PlayerDto.java
+++ src/main/java/com/luqman/fpl_analytics/external/fpl/dto/PlayerDto.java
@@ -1,4 +1,5 @@
 package com.luqman.fpl_analytics.external.fpl.dto;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -6,10 +7,18 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PlayerDto {
+
     private Integer id;
+
+    @JsonProperty("web_name")
     private String web_name;
+
     private Integer team;
+
+    @JsonProperty("now_cost")
     private Integer now_cost;
+
+    @JsonProperty("total_points")
     private Integer total_points;
 
 
