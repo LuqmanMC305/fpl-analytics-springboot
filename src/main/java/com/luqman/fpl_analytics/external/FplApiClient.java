12d1f98b206ae3be35a24d20dca025a5f0fb5e1b
LuqmanMC305 <luqmanulhakim0003@gmail.com>
2026-09-06 00:07:26 +0800

    Modified fplapiclient to use bootstrap response

--- src/main/java/com/luqman/fpl_analytics/external/FplApiClient.java
+++ src/main/java/com/luqman/fpl_analytics/external/FplApiClient.java
@@ -3,12 +3,16 @@ package com.luqman.fpl_analytics.external;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
+import com.luqman.fpl_analytics.external.fpl.dto.BootstrapResponse;
+
 @Component 
 public class FplApiClient {
 
     private final RestClient restClient = RestClient.create();
     
-    public String getRawData(){
+    
+    public BootstrapResponse getBootstrapData(){
+
         return restClient.get()
         .uri("https://fantasy.premierleague.com/api/bootstrap-static/")
         .retrieve()
