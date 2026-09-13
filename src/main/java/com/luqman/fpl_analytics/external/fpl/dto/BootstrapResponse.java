6fe7f8a6d2241dd92c81772354ee489ad6cf93d8
LuqmanMC305 <luqmanulhakim0003@gmail.com>
2026-09-13 13:08:33 +0800

    Added top class-level comment

--- src/main/java/com/luqman/fpl_analytics/external/fpl/dto/BootstrapResponse.java
+++ src/main/java/com/luqman/fpl_analytics/external/fpl/dto/BootstrapResponse.java
@@ -1,5 +1,11 @@
 package com.luqman.fpl_analytics.external.fpl.dto;
 
+/**
+ * Represents top-level response returned by FPL
+ * bootstrap-static API endpoint
+ * Contains player and team information
+ */
+
 import lombok.Setter;
 import lombok.Getter;
 import java.util.List;
