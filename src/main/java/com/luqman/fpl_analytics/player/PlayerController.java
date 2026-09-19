33935db62431717a68266153fc512615534530a0
LuqmanMC305 <luqmanulhakim0003@gmail.com>
2026-09-20 00:21:08 +0800

    Imported logger

--- src/main/java/com/luqman/fpl_analytics/player/PlayerController.java
+++ src/main/java/com/luqman/fpl_analytics/player/PlayerController.java
@@ -11,9 +11,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import com.luqman.fpl_analytics.external.fpl.dto.PlayerDto;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 @RequestMapping("/player")
