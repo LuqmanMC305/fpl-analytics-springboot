48153a23388bec3afe352f46455f14432aae011d
LuqmanMC305 <luqmanulhakim0003@gmail.com>
2026-09-19 22:58:17 +0800

    Added summary comment to PlayerController

--- src/main/java/com/luqman/fpl_analytics/player/PlayerController.java
+++ src/main/java/com/luqman/fpl_analytics/player/PlayerController.java
@@ -1,5 +1,11 @@
 package com.luqman.fpl_analytics.player;
 
+/**
+ * Handles communication with the FPL API.
+ * Retrieves raw player and team data and converts the API response
+ * into application DTOs for further processing.
+ */
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
